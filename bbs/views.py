fc50c1bdbc2ebc3a4d06c26b2edcea2a17f24f77
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-05-23 15:11:35 +0900

    add 検索フォームの実装中

--- bbs/views.py
+++ bbs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Article
 from django.urls import reverse_lazy
+from .forms import SearchForm
 
 class IndexView(generic.ListView):
     model = Article
@@ -25,3 +26,12 @@ class DeleteView(generic.DeleteView):
     model = Article
     template_name = 'bbs/delete.html'
     success_url = reverse_lazy('bbs:index')
+
+def search(request):
+    articles = None
+    searchform = SearchForm(request.GET)
+
+    if searchform.is_valid():
+        query = searchform.cleaned_data['words']
+        articles = Article.objects.filter(content__icontains=query)
+        return render(request, 'bbs/results.html', {'articles':articles, 'searchform':searchform})
