7f429f21c253814b530f5dc4c1017c4068ea3777
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-05-23 13:04:52 +0900

    add ブートストラップ、検索機能実装まで

--- bbs/views.py
+++ bbs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Article
 from django.urls import reverse_lazy
+from .forms import SearchForm
 
 class IndexView(generic.ListView):
     model = Article
@@ -25,3 +26,13 @@ class DeleteView(generic.DeleteView):
     model = Article
     template_name = 'bbs/delete.html'
     success_url = reverse_lazy('bbs:index')
+
+def search(request):
+    articles = None
+    searchform = SearchForm(request.GET)
+
+    if searchform.is_valid():
+        query = searchform.cleaned_data['words'] # フォームに入ってるデータを代入
+        articles = Article.objects.filter(content__icontains=query)
+        print(f"articles:{articles}")
+        return render(request, 'bbs/result.html', {'articles':articles, 'searchform':searchform})
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bbs.apps.BbsConfig'
+    'bbs.apps.BbsConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bbs.context_processors.search_form',
             ],
         },
     },
