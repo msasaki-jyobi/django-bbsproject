c966d6075ad1b3fbf7576c02ef091c48875bc9b1
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-05-26 15:08:22 +0900

    add 検索フォーム実装、ログイン、ログアウト機能実装

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bbs/', include('bbs.urls')),
-    path('', RedirectView.as_view(url='/bbs/'))
+    path('', RedirectView.as_view(url='/bbs/')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
