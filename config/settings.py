dcef239040ef63c9c3801ed758e1e1ac4e6add8a
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-06-03 14:59:17 +0900

    Update ALLOWEDHOSTS ワイルドカード

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-f-b(goolejc1y-^k%p0&*m!&qg_5b$x0zalxotme0n=@few*=^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
