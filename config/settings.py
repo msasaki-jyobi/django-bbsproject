c801344e248901863b1b7fe124c3d8cd29b45460
msasaki-jyobi <103621607+msasaki-jyobi@users.noreply.github.com>
2025-06-05 15:12:40 +0900

    update staticroot

--- config/settings.py
+++ config/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static'] # 静的ファイル用のディレクトリを追記
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 追加
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
